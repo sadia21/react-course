cad20a5caaee727853156d291711bac5303b4ed0
sadia21 <61055917+sadia21@users.noreply.github.com>
2021-11-04 15:36:43 +0100

    moved to Hook folder

---
+++ src/hooks/State.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Statetutorial() {
+  const [counter, setcounter] = useState(0);
+
+  function Increment() {
+    setcounter(counter + 1);
+  }
+  return (
+    <div className="card">
+      <div>
+        {counter}
+        <button className="btn" onClick={Increment}>
+          Increment by one
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Statetutorial;
