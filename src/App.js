cd26aae6e913cec60dcd584908c30543a7c3f0c7
sadia21 <61055917+sadia21@users.noreply.github.com>
2021-11-04 14:44:42 +0100

    Todo App with useStateHook to increment value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Statetutorial from "./component/State.js";
+import Todo from "./component/Todo.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TO DO</h1>
+      <Todo text="Learn JS" />
+      <Todo text="Learn JS 2" />
+      <Todo text="Learn JS 3" />
+      <Statetutorial />
     </div>
   );
 }
